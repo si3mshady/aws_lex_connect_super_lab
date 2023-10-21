57c18f7361b661f897e82871c584512e13ba3f85
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-10-21 12:55:32 -0500

    training bot

--- main.tf
+++ main.tf
@@ -172,40 +172,39 @@ resource "aws_lex_intent" "order_food" {
       max_attempts = 2
 
       response_card = jsondecode({
-    version = 1,
-    content = [
-      {
-        "contentType": "application/vnd.amazonaws.card.generic",
-        "genericAttachments": [
-          {
-            "title": "Food Items",
-            "subTitle": "On the menu",
-            "imageUrl": "vhttps://images.getbento.com/accounts/3bf33a8c2df2466870e9b332ae826067/media/images/smdp2l2o.jpeg?w=1000&fit=max&auto=compress,format&h=1000",
-          
-            "buttons": [
-              {
-                "text": "Juicy Burger",
-                "value": "Burger"
-              },
-               {
-                "text": "Hot Pizza",
-                "value": "Pizza"
-              }, {
-                "text": "Delicious Pasta",
-                "value": "Pasta"
-              }, {
-                "text": "Fresh Salad",
-                "value": "Salad"
-              }, {
-                "text": "More Info",
-                "value": "burger"
-              }
+            version = 1,
+            content = [
+            {
+                "contentType": "application/vnd.amazonaws.card.generic",
+                "genericAttachments": [
+                {
+                    "title": "Food Items",
+                    "subTitle": "On the menu",
+                    # "imageUrl": "https://images.getbento.com/accounts/3bf33a8c2df2466870e9b332ae826067/media/images/smdp2l2o.jpeg?w=1000&fit=max&auto=compress,format&h=1000",
+                
+                    "buttons": [
+                    {
+                        "text": "Juicy Burger",
+                        "value": "Burger"
+                    },
+                    {
+                        "text": "Hot Pizza",
+                        "value": "Pizza"
+                    }, 
+                    {
+                        "text": "Delicious Pasta",
+                        "value": "Pasta"
+                    },
+                    {
+                        "text": "Fresh Salad",
+                        "value": "Salad"
+                    }
+                    ]
+                }
+                ]
+            }
             ]
-          }
-        ]
-      }
-    ]
-  })
+        })
 
       message {
         content_type = "PlainText"
