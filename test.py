cb787d9b6bdce7aff58dbff60e0234f4af0e9d55
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-10-21 16:08:15 -0500

    clean slate

--- test.py
+++ test.py
@@ -1,61 +1,121 @@
-import boto3
-import json
+# import boto3
+# import json
 
-# Create a client for the Amazon Lex Model Building Service
-client = boto3.client('lex-models')
+# # Create a client for the Amazon Lex Model Building Service
+# client = boto3.client('lex-models')
 
-# Define the response card
-response_card = {
-    "version": 1,
-    "content": [
-        {
-            "contentType": "application/vnd.amazonaws.card.generic",
-            "genericAttachments": [
-                {
-                    "title": "Food Items",
-                    "subTitle": "On the menu",
-                    "buttons": [
-                        {
-                            "text": "Burger",
-                            "value": "burger"
-                        },
-                        {
-                            "text": "Pizza",
-                            "value": "pizza"
-                        },
-                        {
-                            "text": "Pasta",
-                            "value": "pasta"
-                        },
-                        {
-                            "text": "Salad",
-                            "value": "salad"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}
+# # Specify the name of the intent you want to retrieve the checksum for
+# intent_name = 'OrderFood'
 
-# Create or update the intent with the response card
-response = client.put_intent(
-    name='OrderFood',
-    slots=[
-        {
-            "name": "OrderItems",
-            "slotConstraint": "Optional",  # Required slotConstraint
-            "valueElicitationPrompt": {
-                "messages": [
-                    {
-                        "contentType": "PlainText",
-                        "content": "What food items would you like to order?"
-                    }
-                ],
-                "maxAttempts": 2  # Required maxAttempts
-            },
-        }
-    ]
-)
 
-print(response)  # Optional: Print the response for verification
+# # Get the intent
+# response = client.get_intent(
+#     name=intent_name,
+#     version='$LATEST'
+# )
+
+# # Get the checksum
+# checksum = response['checksum']
+# print(checksum)
+# # Define the response card
+# response_card = {
+#     "version": 1,
+#     "content": [
+#         {
+#             "contentType": "application/vnd.amazonaws.card.generic",
+#             "genericAttachments": [
+#                 {
+#                     "title": "Food Items",
+#                     "subTitle": "On the menu",
+#                     "buttons": [
+#                         {
+#                             "text": "Burger",
+#                             "value": "burger"
+#                         },
+#                         {
+#                             "text": "Pizza",
+#                             "value": "pizza"
+#                         },
+#                         {
+#                             "text": "Pasta",
+#                             "value": "pasta"
+#                         },
+#                         {
+#                             "text": "Salad",
+#                             "value": "salad"
+#                         }
+#                     ]
+#                 }
+#             ]
+#         }
+#     ]
+# }
+
+# # Define the fulfillment activity
+# fulfillment_activity = {
+#     "type": "ReturnIntent"
+# }
+
+# # Create or update the intent with the fulfillment activity
+# # response = client.put_slot_type(
+# #     name='OrderFood',
+# #     enumerationValues=[
+# #         {
+# #             'value': 'burger'
+# #         },
+# #         {
+# #             'value': 'pizza'
+# #         },
+# #         {
+# #             'value': 'pasta'
+# #         },
+# #         {
+# #             'value': 'salad'
+# #         }
+# #     ]
+# # )
+# # slot_type_version = response['version']
+
+# # response = client.get_slot_type(
+# #     name='OrderFood',
+# #     version='$LATEST'
+# # )
+
+# # # Get the checksum
+# # checksum = response['checksum']
+
+
+# # Define the intent with the custom slot type and slot type version
+# response = client.put_intent(
+#     name='OrderFood',
+#     sampleUtterances=[
+#         'I want to order food.',
+#         'Can I get some food from your restaurant.',
+#         'I\'d like to place an order for delivery.'
+#     ],
+#     slots=[
+#         {
+#             "name": "OrderItems",
+#             "slotType": "MenuOptions",
+#             "slotTypeVersion": "$LATEST",
+#             "slotConstraint": "Optional",
+#             "valueElicitationPrompt": {
+#                 "messages": [
+#                     {
+#                         "contentType": "PlainText",
+#                         "content": "What food items would you like to order?"
+#                     }
+#                 ],
+#                 "maxAttempts": 2
+#             },
+#         }
+#     ],
+#     fulfillmentActivity={
+#         "type": "ReturnIntent"
+#     },
+#     checksum=checksum
+# )
+
+
+
+# print(response)
