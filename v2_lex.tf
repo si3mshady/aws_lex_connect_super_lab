95cd055ff53e1c57cb887bb11346ec251b91d631
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-10-22 04:52:44 -0500

    v2

--- v2_lex.tf
+++ v2_lex.tf
@@ -55,8 +55,11 @@ resource "aws_cloudformation_stack" "lex_bot_cfn" {
 resource "null_resource" "create-bot-locale" {
   depends_on = [ aws_cloudformation_stack.lex_bot_cfn ]
   provisioner "local-exec" {
-    command = <<-EOT
-        aws lexv2-models create-bot-locale --bot-id ${var.bot_id} --bot-version ${var.bot_version} --locale-id ${var.bot_locale} --nlu-intent-confidence-threshold 0.4
+    command = <<EOT
+        aws lexv2-models create-bot-locale --bot-id ${var.bot_id} \
+        --bot-version ${var.bot_version} \
+        --locale-id ${var.bot_locale} \
+        --nlu-intent-confidence-threshold 0.4
     EOT
   }
 }
