e54cf751615f621346ce1029a011938ad0a484a5
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-10-21 19:09:34 -0500

    clean slate

--- main.tf
+++ main.tf
@@ -313,7 +313,7 @@ resource "aws_lambda_function" "test_lambda" {
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
   function_name = "lex_integration"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_handler.lambda_handler"
 
 #   source_code_hash = data.archive_file.lambda.output_base64sha256
